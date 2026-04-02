d3aaab08e1cf5f88b2d7785ca6fea00aa2a8202b
Oscar Filho <oscar.assuncao.filho@gmail.com>
2026-04-01 21:55:26 -0300

    Lock C++ Class made and BP_GlassStatue made also. Collision studies begun.

---
+++ Source/DungeonEscape/Lock.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Lock.h"
+
+// Sets default values
+ALock::ALock()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	Tags.Add("Lock");
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("Root Comp"));
+	SetRootComponent(RootComp);
+
+	TriggerComp = CreateDefaultSubobject<UTriggerComponent>(TEXT("Trigger Comp"));
+	TriggerComp->SetupAttachment(RootComp);
+
+	KeyItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Key Item Mesh"));
+	KeyItemMesh->SetupAttachment(RootComp);
+}
+
+// Called when the game starts or when spawned
+void ALock::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALock::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
